f330dcadb0bd8d2d389860c44850af0c004eb46e
Shubham <25881429+shoebham@users.noreply.github.com>
2024-04-06 17:49:54 +0530

    init fiber

--- app/setup.go
+++ app/setup.go
@@ -1,11 +1,22 @@
 package app
 
-import "splitwise-backend/config"
+import (
+	"log"
+	"splitwise-backend/config"
+
+	"github.com/gofiber/fiber/v3"
+)
 
 func Init() error {
 	err := config.LoadEnv()
 	if err != nil {
 		return err
 	}
+	app := fiber.New()
+
+	app.Get("/", func(c fiber.Ctx) error {
+		return c.SendString("Hello World")
+	})
+	log.Fatal(app.Listen(":3000"))
 	return nil
 }
