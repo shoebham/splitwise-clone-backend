9fede84fbb609dc74a6fe4619b2e2859e98a9fc0
Shubham <25881429+shoebham@users.noreply.github.com>
2024-07-17 00:15:24 +0530

    add todo

--- models/group.go
+++ models/group.go
@@ -12,3 +12,11 @@ type Group struct {
 	GroupName string `json:"GroupName" sql:"group_name"`
 	Members   []int  `json:"Members" sql:"members"`
 }
+
+// TODO
+// add balances column in group user->{owes/owed to,
+//+- share} where user owes or is owed to another user indicated by + or
+//- balance
+// for ex. 1->{2,-100}
+// this means user 1 owes 100 to user 2 in this particular group
+// remove isSettled from expense
