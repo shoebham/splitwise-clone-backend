06f5c3af27d4f1248a18c8a7618392d712324ac9
Shubham <25881429+shoebham@users.noreply.github.com>
2024-07-17 20:55:59 +0530

    fix struct to support balance within group and add support for simplify debt

--- models/group.go
+++ models/group.go
@@ -5,17 +5,25 @@ package models
 	    GroupID,
 	    GroupName,
 	    List<User> Members
+		map<User,Pair<user,Share>> Balances
+		IsSimplify boolean
 	}
 */
+type Balance struct {
+	UserId string `json:"user_id"`
+	Amount int    `json:"amount"`
+}
 type Group struct {
-	Gid       int    `json:"Gid" sql:"gid"`
-	GroupName string `json:"GroupName" sql:"group_name"`
-	Members   []int  `json:"Members" sql:"members"`
+	Gid        int               `json:"Gid" sql:"gid"`
+	GroupName  string            `json:"GroupName" sql:"group_name"`
+	Members    []int             `json:"Members" sql:"members"`
+	Balances   map[int][]Balance `json:"Balances" sql:"balances"`
+	IsSimplify bool              `json:"IsSimplify" sql:"is_simplify"`
 }
 
 // TODO
-// add balances column in group user->{owes/owed to,
-//+- share} where user owes or is owed to another user indicated by + or
+// add balances column in group user->{owes/owed to, +- share}
+//where user owes or is owed to another user indicated by + or
 //- balance
 // for ex. 1->{2,-100}
 // this means user 1 owes 100 to user 2 in this particular group
